2180950664e59a0945b00123827265369c56702b
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 14:35:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,21 @@
 def prime?(n)
+  
   if n < 2 
-    return false 
+    return false
+    
   elsif n == 2 || 3
     return true 
+    
   elsif n >= 2 
     divisor = 2 
     while divisor <= n 
       if n % divisor == 0 
         return false
       divisor += 1  
-    
+      
   else   
     return true
+    
  end
 
 
