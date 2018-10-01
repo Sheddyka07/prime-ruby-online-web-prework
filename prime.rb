1edfbf0616939340203f82a49391fd6dc22c72de
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 14:41:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
-
-def prime?(n)
-  if n < 2 
-    return false 
-  elsif n == 2 || 3 
-    return true
-  elsif (2..Math.sqrt(number_to_test)).each do |num|
-    number_to_test % num == 0  
-    return false 
-  else 
-    return true
-  end 
-end 
+def prime?(number_to_test)
+  return false if number_to_test < 2
+  (2..Math.sqrt(number_to_test)).each do |num|
+    return false if number_to_test % num == 0
+  end
+  true
+end
