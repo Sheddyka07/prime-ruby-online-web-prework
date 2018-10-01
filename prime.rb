e6e57716b0811320297cfff810ad3a4dff69640a
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 14:34:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,20 +4,15 @@ def prime?(n)
   elsif n == 2 || 3
     return true 
   elsif n >= 2 
-    for x in range (2, n-1)
-      if n % x == 0
+    divisor = 2 
+    while divisor <= n 
+      if n % divisor == 0 
         return false
+      divisor += 1  
+    
   else   
     return true
  end
 
 
-elif x >= 2:
-    divisor = 2
-    while divisor <= int(x**0.5+1): #careful with the obiwan
-        if x % divisor == 0:
-            print '%d is not prime. I divided it by %d' % (x, divisor)
-            return False
-        divisor += 1
-    else:
-        return True
+   
