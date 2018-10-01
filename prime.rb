7f56093c9fa0a93c83d40b7ba3dbc41baeee30aa
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 14:16:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime?(testNumber)
+  return false if testNumber < 2 
+  (2..Math.sqrt(testNumber)).each do |int|
+    return false if testNumber % |int| == 0 
+  end
+  true
+end   
